83e4b19c324d475a6f2ea399763e8c9a4d4dc2bf
webdev-710 <scar20181228@gmail.com>
2020-09-25 10:55:08 -0700

    all controllers and models together with view files

--- app/User.php
+++ app/User.php
@@ -36,4 +36,32 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($user) {
+            $user->profile()->create([
+                'title' => $user->name,
+            ]);
+
+            // Mail::to($user->email)->send(new NewUserWelcomeMail());
+        });
+    }
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class)->orderBy('created_at', 'DESC');
+    }
+
+    public function following()
+    {
+        return $this->belongsToMany(Profile::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::auth();
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::post('follow/{user}', 'FollowsController@store');
+
+Route::get('/', 'PostsController@index');
+Route::get('/p/create', 'PostsController@create');
+Route::post('/p', 'PostsController@store');
+Route::get('/p/{post}', 'PostsController@show');
+
+Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
+Route::get('/profile/{user}/edit', 'ProfilesController@edit')->name('profile.edit');
+Route::patch('/profile/{user}', 'ProfilesController@update')->name('profile.update');
